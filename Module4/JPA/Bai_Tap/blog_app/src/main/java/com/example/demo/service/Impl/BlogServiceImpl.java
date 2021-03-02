3ad4f93f66c0323f13a7cce1bb187d3a39b990bb
NguyenThanhTung <tungnguyenthanh24111995@gmail.com>
2021-03-02 10:05:34 +0700

    BaiTap

--- Module4/JPA/Bai_Tap/blog_app/src/main/java/com/example/demo/service/Impl/BlogServiceImpl.java
+++ Module4/JPA/Bai_Tap/blog_app/src/main/java/com/example/demo/service/Impl/BlogServiceImpl.java
@@ -1,7 +1,8 @@
-package com.example.demo.service;
+package com.example.demo.service.Impl;
 
 import com.example.demo.model.Blog;
 import com.example.demo.repository.BlogRepository;
+import com.example.demo.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
